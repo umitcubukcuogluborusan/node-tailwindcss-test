d54357c2960095924487f3035c0517861404650f
Umit CUBUKCUOGLU <ucubukcuoglu@borusan.com>
2024-12-02 15:28:46 +0300

    feat: update port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Statik dosyaları serve et
 app.use(express.static(path.join(__dirname, 'dist')));
